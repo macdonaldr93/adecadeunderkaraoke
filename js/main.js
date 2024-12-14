7774284759712f92c21548b0023a56a40590eeb5
Ryan Macdonald <hi@ryanmacdonald.ca>
2024-12-14 17:08:48 -0500

    perf: use passive on touchstart

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 (() => {
+  const supportsPassive = testPassiveSupport();
+
+  const Selectors = {
+    ListItemPressables: '.links-list__item > a, .links-list__item > button',
+  };
+
+  document.querySelectorAll(Selectors.ListItemPressables).forEach(el => {
+    el.addEventListener('mouseenter', randomizeLinksListItemBoxShadow);
+    el.addEventListener('mouseleave', resetLinksListItemBoxShadow);
+    el.addEventListener(
+      'touchstart',
+      randomizeLinksListItemBoxShadow,
+      supportsPassive
+        ? {
+            passive: true,
+          }
+        : false,
+    );
+    el.addEventListener('touchend', resetLinksListItemBoxShadow);
+  });
+
+  MicroModal.init();
+
+  // Functions
+
   function randomizeLinksListItemBoxShadow(event) {
     const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
       Math.random() * 256,
@@ -11,16 +36,21 @@
     event.target.style.boxShadow = `var(--c-overlay) 8px 8px 0px 0px`;
   }
 
-  const Selectors = {
-    ListItemPressables: '.links-list__item > a, .links-list__item > button',
-  };
+  // Polyfills
 
-  document.querySelectorAll(Selectors.ListItemPressables).forEach(el => {
-    el.addEventListener('mouseenter', randomizeLinksListItemBoxShadow);
-    el.addEventListener('mouseleave', resetLinksListItemBoxShadow);
-    el.addEventListener('touchstart', randomizeLinksListItemBoxShadow);
-    el.addEventListener('touchend', resetLinksListItemBoxShadow);
-  });
+  function testPassiveSupport() {
+    let supportsPassive = false;
 
-  MicroModal.init();
+    try {
+      var opts = Object.defineProperty({}, 'passive', {
+        get: function () {
+          supportsPassive = true;
+        },
+      });
+      window.addEventListener('testPassive', null, opts);
+      window.removeEventListener('testPassive', null, opts);
+    } catch (e) {}
+
+    return supportsPassive;
+  }
 })();
